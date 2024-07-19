8104ed758eb13cdb9e579c976470eab9a11ca7ee
Matheus Moreno Sá <mms.matheus1@gmail.com>
2024-07-19 10:35:28 -0300

    update

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import random
 import seaborn as sns
 import streamlit.components.v1 as components
 
-apply_update = False
+apply_update = True
 
 st.set_page_config(
         page_title="Previsão Brasileirão",
